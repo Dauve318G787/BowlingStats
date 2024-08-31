2af317758024a1c990c2e151336e3921df7065d7
Dauve318G787 <davbekdup@gmail.com>
2024-08-31 12:48:28 -0500

    Added bowlers for 2024 season

--- indexjs.js
+++ indexjs.js
@@ -17,19 +17,43 @@ function updateBowlers()
     //get selected season from season dropdown
     const seasonSelection = seasonDropdown.value;
 
-    //define bowlers based on selected season
+    //populate list of bowlers based on season
     let bowlers = [];
     if (seasonSelection == "2024")
         {
-        bowlers = ["Please select a season", "AJ Chapman"];
+        bowlers = ["Ryan Barnes", "Chris Via", 
+            "Nate Stubler", "Bill O'Neill",
+            "Tom Smallwood", "EJ Tackett", "Kyle Troup",
+            "Jason Belmonte", "Anthony Simonsen",
+            "Jake Peters", "Sean Lavery-Spahr",
+            "Marshall Kent", "Matt Russo",
+            "Benjy Martinez", "Zach Wilkins",
+            "Tim Foy Jr.", "David Krol",
+            "Tomas Kayhko", "Sam Cooley",
+            "Richie Teece", "Patrick Dombrowski",
+            "DeeRonn Booker", "BJ Moore", 
+            "Alec Keplinger", "Deo Benard",
+            "Mikey Schlabach", "Jesper Svensson",
+            "Packy Hanrahan", "Keven Williams",
+            "Kris Prather", "Shota Kawazoe",
+            "Michael Martell", "Kyle Sherman",
+            "Eric Jones", "Justin Knowles",
+            "Graham Fach", "Jason Sterner",
+            "Matt Ogle", "Andrew Anderson"];
         }
+    
+    //sort bowlers list alphabetically
+    bowlers.sort();
 
     //populate bowler dropdown with bowlers for that season
     bowlers.forEach(bowler => 
         {
+        //create option object
         const option = document.createElement("option");
+        //populate option object
         option.value = bowler;
         option.textContent = bowler;
+        //append option object to bowler dropdown
         bowlerDropdown.appendChild(option);
         });
     }
